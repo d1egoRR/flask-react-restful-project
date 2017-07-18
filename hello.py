1be39d0f331f91d55e333a8219d98919c9e9c1cf
you <diegoriquelme81@gmail.com>
2017-07-18 12:11:58 -0300

    pymongo with flask init

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from pymongo import MongoClient
+from flask_restful import Resource, Api
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+app = Flask('webflask')
+api = Api(app)
 
-if __name__ == "__main__":
-    app.run()
+client = MongoClient('localhost', 27017)    #Configure the connection to the database
+db = client.webflask    #Select the database
+users_mongo = db.users #Select the collection
+
+class HelloWorld(Resource):
+    def get(self):
+      users = users_mongo.find()
+      #listado = []
+      listado = {}
+      for user in users:
+        #listado.append({'name': user['name']})
+        listado['name'] = user['name']
+      return listado
+
+api.add_resource(HelloWorld, '/sample')
+
+if __name__ == '__main__':
+    app.run(debug=True)
