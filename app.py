959c58b9934a982cda3e055be451f8a44f3bed3c
you <diegoriquelme81@gmail.com>
2017-07-20 20:04:34 -0300

    general refactoring in test and app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 from bson.objectid import ObjectId
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask.views import View
+from flask_restful import Resource, Api, reqparse, abort
 from pymongo import MongoClient
-from flask_restful import Resource, Api, reqparse
 
 app = Flask('blog')
 api = Api(app)
@@ -12,29 +14,28 @@ client = MongoClient('localhost', 27017)
 db = client.blog
 blog_posts = db.posts
 
-parser = reqparse.RequestParser()
-parser.add_argument('title')
-parser.add_argument('author')
-parser.add_argument('text_post')
+parser = reqparse.RequestParser(bundle_errors=True)
+parser.add_argument('title', type=str, required=True, help="No debe estar vacio")
+parser.add_argument('author', type=str, required=True, help="No debe estar vacio")
+parser.add_argument('text_post', type=str, required=True, help="No debe estar vacio")
 parser.add_argument('comment_author')
 parser.add_argument('comment_text')
 
-@app.route("/posts/create")
-def post_form():
-    return render_template('index.html')
-    
+
+class PostCreate(View):
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def dispatch_request(self):
+        return render_template(self.template_name)
+
+app.add_url_rule(
+    '/posts/create',
+    view_func=PostCreate.as_view('show_posts', 'index.html'))
+
 
 class PostList(Resource):
     def get(self):
-        post_list = self.get_posts_list()
-        return post_list
-
-    def post(self):
-        args = parser.parse_args()
-        result = self.save_post(args)
-        return result
-
-    def get_posts_list(self):
         posts = blog_posts.find()
         post_list = []
         for post in posts:
@@ -43,34 +44,40 @@ class PostList(Resource):
             post_list.append(post)
         return post_list
 
-    def save_post(self, data):
-        if data['title'] == '' or data['author'] == '' or data['text_post'] == '':
-            return False
+
+class PostSave(Resource):
+    def post(self):
+        args = parser.parse_args()
+        if args['title'] == '' or args['author'] == '' or args['text_post'] == '':
+            return {'result': False}
 
         post = {}
-        post['title'] = data['title']
-        post['author'] = data['author']
-        post['text_post'] = data['text_post']
+        post['title'] = args['title']
+        post['author'] = args['author']
+        post['text_post'] = args['text_post']
         post['date'] = datetime.utcnow()
 
         blog_posts.insert_one(post)
+        return {'result': True}
 
-        return True
-
-class Post(Resource):
+class PostById(Resource):
     def get(self, post_id):
-        post = self.get_post_by_id(post_id)
-        return post
+        if not ObjectId.is_valid(post_id):
+            return {'message': 'El ID no es valido.'}
 
-    def get_post_by_id(self, post_id):
         post = blog_posts.find_one({"_id": ObjectId(post_id)})
+
+        if post is None:
+            return {'message': 'No existe el post.'}
+
         post['_id'] = str(post['_id'])
         post['date'] = str(post['date'])
         return post
 
 
-api.add_resource(PostList, '/posts')
-api.add_resource(Post, '/posts/<string:post_id>')
+api.add_resource(PostList, '/api/posts')
+api.add_resource(PostSave, '/api/posts/save')
+api.add_resource(PostById, '/api/posts/<string:post_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import tempfile
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import json
+import unittest
 
 from bson.objectid import ObjectId
 from datetime import datetime
-import json
-import unittest
 from pymongo import MongoClient
 
 import app
@@ -47,8 +47,8 @@ class TestBlog(unittest.TestCase):
     def tearDown(self):
         self.client.drop_database('test_blog')
 
-    def test_get_postlist(self):
-        response = self.app.get('/posts')
+    def test_get_postslist(self):
+        response = self.app.get('/api/posts')
         result = json.loads(response.get_data())
 
         self.assertEqual(len(result), 2)
@@ -56,9 +56,16 @@ class TestBlog(unittest.TestCase):
         self.assertEqual(result[0]['author'], 'Test')
 
     def test_get_with_param_id(self):
-        response = self.app.get('/posts/606e7ba15265ab2bd33bfaba')
+        response = self.app.get('/api/posts/606e7ba15265ab2bd33bfabxx')
         result = json.loads(response.get_data())
+        self.assertEqual(result['message'], 'El ID no es valido.')
 
+        response = self.app.get('/api/posts/706e7ba15265ab2bd33bfaba')
+        result = json.loads(response.get_data())
+        self.assertEqual(result['message'], 'No existe el post.')
+
+        response = self.app.get('/api/posts/606e7ba15265ab2bd33bfaba')
+        result = json.loads(response.get_data())
         self.assertEqual(result['title'], 'Title Test 2')
         self.assertEqual(result['author'], 'Test2')
 
