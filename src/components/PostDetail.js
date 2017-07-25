cc124f54f767dfb8b6ea6bd101d20862973b734f
you <diegoriquelme81@gmail.com>
2017-07-25 11:20:50 -0300

    Add comments in the posts

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -14,19 +14,41 @@ export default class PostDetail extends React.Component {
 
   componentDidMount() {
     getPost(this.state.id, post => {
-      this.setState({post: post});
-    });
+      this.setState({post: post});    });
   }
 
   render() {
+    const currentPost = this.state.post;
+    let commentList = '';
+
+    if (currentPost['comments']) {
+      commentList = currentPost.comments.map(function(comment, index) {
+        return(
+          <Row key={index}>
+            {comment.text_comment} -- 
+            {comment.author}
+          </Row>
+        );
+      });
+    }
+
     return(
       <Grid>
         <Row>
           <Col>
             <Well>
-              <h6>{this.state.post.title}</h6>
+              <h1>{this.state.post.title}</h1>
               <p>{this.state.post.text_post}</p>
               <Link to='/'>Volver</Link>
+            </Well>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Well>
+              <h3>Comentarios</h3>
+              <hr></hr>
+              {commentList}
             </Well>
           </Col>
         </Row>
