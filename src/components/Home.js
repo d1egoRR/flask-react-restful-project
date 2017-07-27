9f176267dbcf419be6b441da88e972acbea4fef1
you <diegoriquelme81@gmail.com>
2017-07-27 19:04:38 -0300

    add next and previous pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,6 @@ const LIMIT_POSTS_PER_PAGE = 5;
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    console.log(this.props.match.params.page);
     const page = (this.props.match.params.page)
                ? (this.props.match.params.page)
                : 1;
@@ -21,15 +20,27 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-    getPostsList(this.state.currentPage, result => {
+    this.getPostsListData(this.state.currentPage);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      currentPage: parseInt(nextProps.match.params.page)
+    });
+    this.getPostsListData(nextProps.match.params.page);
+  }
+
+  getPostsListData(page) {
+    getPostsList(page, result => {
       this.setState({
-          posts: result.posts,
-          postsCount: parseInt(result.posts_count)
-        });
+        posts: result.posts,
+        postsCount: parseInt(result.posts_count)
+      });
     });
   }
 
   render() {
+    window.scrollTo(0, 0);
     const totalPages = Math.ceil(this.state.postsCount / LIMIT_POSTS_PER_PAGE);
     const previousPage = (this.state.currentPage > 1)
                        ? (this.state.currentPage - 1)
@@ -37,21 +48,21 @@ export default class Home extends React.Component {
     const nextPage = (totalPages > this.state.currentPage)
                    ? (this.state.currentPage + 1)
                    : false;
-
     return(
       <div>
         <PostsList posts={this.state.posts} />
         <Grid>
           {(previousPage)
             ? <div key={previousPage}>
-                <Link to={`/posts/${previousPage}`} replace >
+                <Link to={`/posts/${previousPage}`} >
                   &larr; Página Anterior
                 </Link>
               </div>
             : ''}
+
           {(nextPage)
             ? <div key={nextPage}>
-                <Link to={`/posts/${nextPage}`} replace >
+                <Link to={`/posts/${nextPage}`} >
                   Página Siguiente &rarr;
                 </Link>
               </div>
